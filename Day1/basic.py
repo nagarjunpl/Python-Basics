759eb12db6a165b3d877560a7ed8cf340fa311f3
Nagarjun PL <nagarjunpl4400@gmail.com>
2025-05-29 21:38:54 +0530

    Add files via upload

---
+++ Day1/basic.py
@@ -0,0 +1,18 @@
+print("Welcome to Python")
+
+# Data type
+
+name = "Nagarjun"  #String
+age = 20  #int
+Weight = 60.5  #Float
+am_a_student = True  # Bool
+
+print(type(name)) #type function is used to check the type of the variable
+
+age_float = float(age) # converting int to float
+
+a="10" #string
+
+print(int(a)+Weight) # converting string to int
+
+
