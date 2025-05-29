0ce7cc3af999ed42784c48b4e95b0a4a81493568
Nagarjun PL <nagarjunpl4400@gmail.com>
2025-05-29 21:49:51 +0530

    Add files via upload

---
+++ Day1/Arithmetic_operator.py
@@ -0,0 +1,16 @@
+# Arithmetic operator
+
+a = 10
+b = 20
+
+print(a + b) #addition
+print(a - b) #subtraction
+print(a * b) #multiplication
+print(a / b) #division
+print(a // b) #floor division
+print(a % b) #modulus
+print(a ** b) #exponentiation
+
+
+print(a+b*a/a*45//20)
+
