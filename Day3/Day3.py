43d19106127128963a291f1d908a02f074d2da60
Nagarjun PL <nagarjunpl4400@gmail.com>
2025-05-31 22:17:08 +0530

    Add files via upload

---
+++ Day3/Day3.py
@@ -0,0 +1,13 @@
+
+# String Manupulation
+
+first_name = "nagarjun"
+last_name = "pl"
+
+print(first_name + " " + last_name)
+
+
+# repititions
+
+name = "Arjun \n"*5  # printing a 5 times
+print(name)
