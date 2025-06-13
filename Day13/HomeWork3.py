a0fdc0aaf81d27086cb15de89a623d483d7b626f
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-06-13 19:57:13 +0530

    Update HomeWork3.py

--- Day13/HomeWork3.py
+++ Day13/HomeWork3.py
@@ -1,6 +1,4 @@
-
 # Ticket Booking System
-
 seats = 8
 
 while seats : # automatically checks if seats is not zero
@@ -20,3 +18,63 @@ while countdown > 0:
     print(countdown)
     time.sleep(0)  # Wait for 1 second
     countdown -= 1
+
+
+# Multiplication Table for 3 to 5
+
+for i in range(3,6): #  # Loop through numbers 3 to 5
+    for j in range(1, 11):  # Loop through numbers 1 to 10
+        print(f"{i} x {j} = {i * j}")
+
+
+# sum of first 10 natural numbers
+
+total = 0
+for i in range(1, 11):  # Loop through numbers 1 to 10
+    
+    total += i  # Add each number to total
+    print(total)  # Print the current total after each addition
+
+print("Sum of first 10 natural numbers:", total)
+
+
+# printing a name letter by letter
+
+name = 'Nagarjun'
+
+for letter in name:
+    print(letter)
+
+
+# counting vowels in a string
+
+vowels = "aeiouAEIOU"
+
+count = 0
+
+text = "Hello, how are you doing today?"
+for letter in text:
+    if letter in vowels:
+        count += 1
+
+print("Number of vowels are :",count)
+
+
+# Sum of prices in a dictionary
+
+dishes = {
+    "soup": 299,
+    "burger": 899,
+    "pizza": 799,
+    "soda": 199
+}
+
+total_price = 0
+for key, value in dishes.items():
+    total_price += value  # Add each dish's price to total_price
+
+print("Total prices :",total_price)
+
+# OR
+
+print(sum(dishes.values()))
