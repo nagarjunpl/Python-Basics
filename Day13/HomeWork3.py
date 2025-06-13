507482a939e4f11d692852cf84c9166c5a017a68
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-06-13 19:55:33 +0530

    Update HomeWork3.py

--- Day13/HomeWork3.py
+++ Day13/HomeWork3.py
@@ -11,3 +11,12 @@ while seats : # automatically checks if seats is not zero
     print(f"Seats left: {seats}")
 print("All seats are booked!")
 
+# countdown timer using time module
+import time
+
+countdown = 10
+print("Countdown starts now!")
+while countdown > 0:
+    print(countdown)
+    time.sleep(0)  # Wait for 1 second
+    countdown -= 1
