d57df528e84d653e2a5ab8999c8ebaa8255cdd7b
Nagarjun P L <nagarjunpl4400@gmail.com>
2025-06-15 19:34:42 +0530

    Add files via upload

---
+++ Day15/Functions.py
@@ -0,0 +1,16 @@
+# Functions
+
+def greet():
+    print("Hello, welcome to the program!")
+
+greet()
+
+
+# Function with parameters
+def marriage(boy,girl):
+    print(f"{boy} married {girl}")
+
+marriage("John", "Jane") #position of arguments
+marriage(boy="chandan", girl="chandu") #keyword arguments
+
+
